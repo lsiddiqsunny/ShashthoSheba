01990964d4a5ee08e9dce17b48785edd2e6833d9
Zangetsu101 <haidertameem@gmail.com>
2020-04-18 19:08:50 +0600

    appointments and home tab

--- lib/tabbedPages.dart
+++ lib/tabbedPages.dart
@@ -5,7 +5,7 @@ import './appointmentsTab.dart';
 import './searchDoctorTab.dart';
 
 class TabbedPages extends StatefulWidget {
-  static const routeName='/tabbedpages';
+  static const routeName = '/tabbedpages';
 
   @override
   _TabbedPagesState createState() => _TabbedPagesState();
@@ -16,13 +16,10 @@ class _TabbedPagesState extends State<TabbedPages>
   final List<Tab> myTabs = <Tab>[
     Tab(
       text: 'Home',
+      icon: Icon(Icons.home),
     ),
-    Tab(
-      text: 'Appointments',
-    ),
-    Tab(
-      text: 'Search Doctor',
-    ),
+    Tab(text: 'Appointments', icon: Icon(Icons.view_list)),
+    Tab(text: 'Search Doctor', icon: Icon(Icons.search)),
   ];
 
   TabController _tabController;
@@ -30,7 +27,10 @@ class _TabbedPagesState extends State<TabbedPages>
   @override
   void initState() {
     super.initState();
-    _tabController = TabController(vsync: this, length: myTabs.length);
+    _tabController = TabController(
+      vsync: this,
+      length: myTabs.length,
+    );
   }
 
   @override
@@ -44,11 +44,54 @@ class _TabbedPagesState extends State<TabbedPages>
     return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: false,
-        bottom: TabBar(controller: _tabController, tabs: myTabs),
+        bottom: TabBar(
+          controller: _tabController,
+          tabs: myTabs,
+        ),
+      ),
+      endDrawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Column(
+                children: <Widget>[
+                  CircleAvatar(
+                    minRadius: 45,
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    'Guest',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('Settings'),
+            ),
+            ListTile(
+              leading: Icon(Icons.exit_to_app),
+              title: Text('Log Out'),
+            ),
+          ],
+        ),
       ),
       body: TabBarView(
         controller: _tabController,
-        children: [HomeTab(),AppointmentsTab(),SearchDoctorTab()],
+        children: [
+          HomeTab(),
+          AppointmentsTab(),
+          SearchDoctorTab(),
+        ],
       ),
     );
   }
