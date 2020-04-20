065278aa41b860c03a7e4b674ddff3719ad3d749
Zangetsu101 <haidertameem@gmail.com>
2020-04-20 13:38:10 +0600

    added some validations

--- lib/registerPage.dart
+++ lib/registerPage.dart
@@ -12,8 +12,9 @@ class RegisterPage extends StatefulWidget {
 
 class _RegisterPageState extends State<RegisterPage> {
   Sex _sex = Sex.male;
-  TextEditingController dob = TextEditingController();
+  TextEditingController _dob = TextEditingController();
   DateTime _selectedDate;
+  final _formKey = GlobalKey<FormState>();
 
   void registerAction(BuildContext context) {
     print('Register button pressed');
@@ -24,138 +25,164 @@ class _RegisterPageState extends State<RegisterPage> {
     return Scaffold(
       appBar: AppBar(),
       body: Center(
-        child: ListView(
-          shrinkWrap: true,
-          children: <Widget>[
-            Card(
-              child: Container(
-                padding: EdgeInsets.all(35),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    Align(
-                      alignment: Alignment.center,
-                      child: Text(
-                        'Register',
-                        style: TextStyle(
-                          fontSize: 28,
+        child: Form(
+          key: _formKey,
+          child: ListView(
+            shrinkWrap: true,
+            children: <Widget>[
+              Card(
+                child: Container(
+                  padding: EdgeInsets.all(35),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      Align(
+                        alignment: Alignment.center,
+                        child: Text(
+                          'Register',
+                          style: TextStyle(
+                            fontSize: 28,
+                          ),
                         ),
                       ),
-                    ),
-                    SizedBox(
-                      height: 25,
-                    ),
-                    TextFormField(
-                      decoration: InputDecoration(
-                        labelText: 'Name',
-                        hasFloatingPlaceholder: true,
+                      SizedBox(
+                        height: 25,
                       ),
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    Row(
-                      children: <Widget>[
-                        Text(
-                          'Gender:',
-                          style: TextStyle(fontSize: 16),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          labelText: 'Name',
+                          hasFloatingPlaceholder: true,
                         ),
-                        Radio(
-                          value: Sex.male,
-                          groupValue: _sex,
-                          onChanged: (Sex value) {
-                            setState(() {
-                              _sex = value;
-                            });
-                          },
-                        ),
-                        Text('Male'),
-                        Radio(
-                          value: Sex.female,
-                          groupValue: _sex,
-                          onChanged: (Sex value) {
-                            setState(() {
-                              _sex = value;
-                            });
-                          },
-                        ),
-                        Text('Female'),
-                      ],
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    TextFormField(
-                      decoration: InputDecoration(
-                        labelText: 'Mobile No.',
-                        hasFloatingPlaceholder: true,
+                        validator: (value) {
+                          if(value.isEmpty) {
+                            return 'Please enter your name';
+                          }
+                          return null;
+                        },
                       ),
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    TextFormField(
-                      readOnly: true,
-                      controller: dob,
-                      decoration: InputDecoration(
-                        labelText: 'Date of Birth',
-                        hasFloatingPlaceholder: true,
-                        suffixIcon: Icon(Icons.date_range),
+                      SizedBox(
+                        height: 20,
                       ),
-                      onTap: () async {
-                        DateTime selectedDate = await showDatePicker(
-                            context: context,
-                            firstDate: DateTime.now().subtract(
-                              Duration(
-                                days: 36500,
+                      Row(
+                        children: <Widget>[
+                          Text(
+                            'Gender:',
+                            style: TextStyle(fontSize: 16),
+                          ),
+                          Radio(
+                            value: Sex.male,
+                            groupValue: _sex,
+                            onChanged: (Sex value) {
+                              setState(() {
+                                _sex = value;
+                              });
+                            },
+                          ),
+                          Text('Male'),
+                          Radio(
+                            value: Sex.female,
+                            groupValue: _sex,
+                            onChanged: (Sex value) {
+                              setState(() {
+                                _sex = value;
+                              });
+                            },
+                          ),
+                          Text('Female'),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          labelText: 'Mobile No.',
+                          hasFloatingPlaceholder: true,
+                        ),
+                        validator: (value) {
+                          if(value.isEmpty) {
+                            return "Please enter your Mobile Number";
+                          }
+                          return null;
+                        },
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      TextFormField(
+                        readOnly: true,
+                        controller: _dob,
+                        decoration: InputDecoration(
+                          labelText: 'Date of Birth',
+                          hasFloatingPlaceholder: true,
+                          suffixIcon: Icon(Icons.date_range),
+                        ),
+                        validator: (value) {
+                          if(value.isEmpty) {
+                            return "Please enter your Date of Birth";
+                          }
+                          return null;
+                        },
+                        onTap: () async {
+                          DateTime selectedDate = await showDatePicker(
+                              context: context,
+                              firstDate: DateTime.now().subtract(
+                                Duration(
+                                  days: 36500,
+                                ),
                               ),
-                            ),
-                            initialDate: _selectedDate == null
-                                ? DateTime.now()
-                                : _selectedDate,
-                            initialDatePickerMode: DatePickerMode.year,
-                            lastDate: DateTime.now());
-                        if (selectedDate != null) {
-                          _selectedDate = selectedDate;
-                          setState(() => dob.text =
-                              DateFormat.yMMMMd('en_US').format(_selectedDate));
-                        }
-                      },
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    TextFormField(
-                      decoration: InputDecoration(
-                        labelText: 'Password',
-                        hasFloatingPlaceholder: true,
+                              initialDate: _selectedDate == null
+                                  ? DateTime.now()
+                                  : _selectedDate,
+                              initialDatePickerMode: DatePickerMode.year,
+                              lastDate: DateTime.now());
+                          if (selectedDate != null) {
+                            _selectedDate = selectedDate;
+                            setState(() => _dob.text =
+                                DateFormat.yMMMMd('en_US')
+                                    .format(_selectedDate));
+                          }
+                        },
                       ),
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    TextFormField(
-                      decoration: InputDecoration(
-                        labelText: 'Confirm Password',
-                        hasFloatingPlaceholder: true,
+                      SizedBox(
+                        height: 20,
                       ),
-                    ),
-                    SizedBox(
-                      height: 15,
-                    ),
-                    Align(
-                      alignment: Alignment.centerRight,
-                      child: RaisedButton(
-                        onPressed: () => registerAction(context),
-                        child: Text('Register'),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          labelText: 'Password',
+                          hasFloatingPlaceholder: true,
+                        ),
                       ),
-                    ),
-                  ],
+                      SizedBox(
+                        height: 20,
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          labelText: 'Confirm Password',
+                          hasFloatingPlaceholder: true,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      Align(
+                        alignment: Alignment.centerRight,
+                        child: RaisedButton(
+                          onPressed: () {
+                            if(_formKey.currentState.validate()) {
+                              registerAction(context);
+                            }
+                          },
+                          child: Text('Register'),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
+                margin: EdgeInsets.only(left: 25, right: 25),
               ),
-              margin: EdgeInsets.only(left: 25, right: 25),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
